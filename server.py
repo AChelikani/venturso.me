49e845bac416f36cb5cb49f3ae72b443645a8036
RobertEng <googlypantsroberteng@gmail.com>
2015-10-10 00:43:15 -0700

    Flask server setup

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def splash():
+    # return 'Hello World!'
+    return render_template('splash.html')
+
+@app.route('/creation')
+def create_tour():
+    # return 'Hello World!'
+    return render_template('creation.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
