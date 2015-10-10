7079d2f533401620fa78d98ed5bccd469a13455b
Advith Chelikani <advith.chelikani@gmail.com>
2015-10-10 03:53:23 -0700

    update helper functions

--- api.py
+++ api.py
@@ -22,20 +22,18 @@ def pollUberPrice(a, b, c, d):
     url = "https://api.uber.com/v1/estimates/price?start_latitude=" + str(a) + "&start_longitude=" + str(b) + "&end_latitude=" + str(c) + "&end_longitude=" + str(d) + "&server_token=dVyCwZlJFJ8pUUg3jagY8ETawAkBnygEq3yduosF"
     r = requests.get(url)
     relevant = r.json()
-    print relevant["prices"][0]["estimate"]
     return relevant["prices"][0]["estimate"]
 
-
+# Params: start lat, start long, end lat, end long
+# Output string of travel time in minutes
 def pollHereTravelTime(a, b, c, d):
     url = "https://route.cit.api.here.com/routing/7.2/calculateroute.json?waypoint0=52.5160%2C13.3779&waypoint1=52.5206%2C13.3862&mode=fastest%3Bcar%3Btraffic%3Aenabled&app_id=N6MJW6UzW079S5ZZwcPl&app_code=FOkZLbFrMx77dDpomCs9ZQ&tf&departure=now"
     r = requests.get(url)
     relevant = r.json()
-    print relevant["response"]["route"][0]
-    return relevant["response"]
+    return relevant["response"]["route"][0]["leg"][0]["travelTime"]
 
-pollHereTravelTime(1, 1, 1, 1)
-
-# pollUber(34.139011, -118.124514, 34.130647, -118.147108)
+# print pollHereTravelTime(1, 1, 1, 1)
+# print pollUber(34.139011, -118.124514, 34.130647, -118.147108)
 # print poll(34.139011, -118.124514)        
 
     
