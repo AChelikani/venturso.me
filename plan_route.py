35926dc80bb2e3b93209d38d8c1b9fbe7601b757
Andrew Kang <akang@caltech.edu>
2015-10-10 15:37:52 -0700

    Updated subset algorithm

--- plan_route.py
+++ plan_route.py
@@ -82,35 +82,38 @@ def make_itinerary_subset(start_lat, start_lng, start_time, end_lat, end_lng, en
 
 	lat, lng = start_lat, start_lng
     activities = api.pollHereAttractions(lat, lng)
-    #print activities
 
     sorted(activities, key = lambda activity: activity.value())
-    num_activities = 5
-    
+
     # Get subsets
+    num_activities = 5
     for act in num_activities:
         act = 
 
     # Find best subset by computing traveling time
-    for candidate in candidates:
+    for candidate in candidates: # candidate is an array of activities
         current_lat = start_lat
         current_lng = start_lng
         min_distance = 1000000 # Ridiculously large number
-        ordered_candidates[candidate] = []
+        ordered_candidate = []
+        ordered_activity_value = 100 # Depends on scoring system
+
         while len(candidate) != 0:
             for activity in candidate:
                 current_distance = distance(current_lat, current_lng, activity.lat, activity.lng)
                 if current_distance < min_distance:
                     min_distance = current_distance
                     best_activity = activity
+            ordered_candidate.append(best_activity)
+            current_lat = best_activity.lat
+            current_lng = best_activity.lng
             del candidate[activity]
-            ordered_candidates[candidate].append(activity)
-            ordered_candidates[candidate] += current_distance + activity.value()
+            ordered_activity_value -= current_distance + best_activity.value()
 
-    
+        ordered_candidates[ordered_candidate] = ordered_activity_value
 
-	add_transportation(itinerary, current_time, lat, lng, end_lat, end_lng)
-
+    itinerary = max(ordered_candidates.iteritems(), key=operator.itemgetter(1))[0]
 	return itinerary
-		
-		#update(current_location, current_time)
+
+
+
